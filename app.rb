a5638af164a86ea9c0facaf36deb671eb98ae374
Siôn Le Roux <sinisterstuf@gmail.com>
2015-06-16 08:01:56 +0200

    Hide inner workings from output, just in case

--- app.rb
+++ app.rb
@@ -6,11 +6,21 @@ set :server, 'thin'
 base_url = ''
 
 get '/' do
-    Net::HTTP.post_form(URI.parse(
+
+    # do a YoAll
+    status = Net::HTTP.post_form(URI.parse(
         'https://api.justyo.co/yoall/'),
         {
             'api_token' => ENV['YO_KEY'],
             'link' => 'http://yotext.co/show/?text=COFFEE TIME?'
         }
     )
+
+    # terse status response
+    if status
+        'success'
+    else
+        'failed to YoAll'
+    end
+
 end
