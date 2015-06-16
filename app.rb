69088d85ba0cde641d6c9504070a7af9578fddc7
Siôn Le Roux <sinisterstuf@gmail.com>
2015-06-16 07:48:01 +0200

    Empty YoAll! getting Yo API key from environment

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
+require 'net/http'
 
 set :server, 'thin'
 
 base_url = ''
 
 get '/' do
-    "COFFEE TIME!"
+    Net::HTTP.post_form(URI.parse(
+        'https://api.justyo.co/yoall/'),
+        {'api_token' => ENV['YO_KEY']}
+    )
 end
