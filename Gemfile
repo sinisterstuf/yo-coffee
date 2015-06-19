d75de22ec2b1e9ff1bc5db0743f24c7be5985f82
Siôn Le Roux <sinisterstuf@gmail.com>
2015-06-20 00:24:24 +0200

    Implement read+write+expire requesters in Redis
    Now we save the requester's names to a set in redis.  Being a set, many
    requests from the same user will only be counted once.  If there's only
    one person, you get their name, otherwise you get the number of people.
    
    Additionally, we set it to expire after a minute, so that if nobody
    responds in a minute then your "coffee request" is cancelled and the
    next request starts counting names from scratch.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.2.2'
 
 gem 'sinatra', '~>1.4.0'
 gem 'thin', '~>1.6.0'
+gem 'redis', '~>3.2.0'
--- app.rb
+++ app.rb
@@ -1,20 +1,39 @@
 require 'sinatra'
 require 'net/http'
+require 'redis'
 
 set :server, 'thin'
-
 base_url = ''
+redis = Redis.new(:url => ENV['REDISTOGO_URL'])
 
 get '/' do
 
-    sender = params["username"]
+    if params.key?('username')
+        logger.info "adding user to redis"
+        redis.sadd('people', params['username'])
+
+        # never mind this coffee session if nobody responds in time
+        redis.expire('people', 60)
+
+    end
+
+    # return name or count depending on card
+    headcount = redis.scard('people')
+    subject =
+        if headcount < 2
+            redis.srandmember('people')
+        else
+            headcount.to_s + "people"
+        end
+
+    text = "#{subject} would like coffee!"
 
     # do a YoAll
     status = Net::HTTP.post_form(URI.parse(
         'https://api.justyo.co/yoall/'),
         {
             'api_token' => ENV['YO_KEY'],
-            'link' => "http://yotext.co/show/?text=#{sender} would like coffee!"
+            'link' => "http://yotext.co/show/?text=" + text,
         }
     )
 
